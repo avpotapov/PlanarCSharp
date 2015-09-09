3dbfe274500d2ed0cd9290a0b54ef9ffb0a47679
Алексей Потапов <al.ptp@hotmail.com>
2015-09-09 16:51:34 +0300

    Добавлены классы Libraries, Library, Module. Проблемы с сериализацией

--- Planar/Library/Library.cs
+++ Planar/Library/Library.cs
@@ -3,10 +3,162 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Reflection;
 
-namespace Library
+
+namespace Planar.Library
 {
+    public sealed class Libraries : Dictionary<Library.TypeLibrary, Library>
+    {
+        private Libraries() { }
+
+        static private Libraries libraries;
+        public static Libraries Instance
+        {
+            get
+            {
+                if (libraries == null)
+                    libraries = new Libraries();
+                return libraries;
+            }
+        }
+
+        public void Add(Library library)
+        {
+            Add(library.Type, library);
+        }
+
+        public void Serializere(string LibraryPath = "")
+        {
+            XmlSerializer librarySerializer;
+            StreamWriter libraryWriter;
+            String FullFileName = "";
+
+            if (LibraryPath == "")
+            {
+                LibraryPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase).LocalPath);
+                LibraryPath = Path.Combine(LibraryPath, "library");
+                if (!Directory.Exists(LibraryPath))
+                    Directory.CreateDirectory(LibraryPath);
+
+            }
+
+            foreach (var library in Values)
+            {
+                librarySerializer = new XmlSerializer(typeof(Library));
+
+                switch (library.Type)
+                {
+                    case Library.TypeLibrary.Vendor:
+                        LibraryPath = Path.Combine(LibraryPath, "vendor");
+                        if (!Directory.Exists(LibraryPath))
+                            Directory.CreateDirectory(LibraryPath);
+                        FullFileName = Path.Combine(LibraryPath, "list.xml");
+                        break;
+
+                    case Library.TypeLibrary.Custom:
+                        LibraryPath = Path.Combine(LibraryPath, "custom");
+                        if (!Directory.Exists(LibraryPath))
+                            Directory.CreateDirectory(LibraryPath);
+                        FullFileName = Path.Combine(LibraryPath, "list.xml");
+                        break;
+                }
+
+                using (libraryWriter = new StreamWriter(FullFileName))
+                    librarySerializer.Serialize(libraryWriter, library);
+            }
+        }
+
+        public void Deserialize(string LibraryPath = "")
+        {
+            if (LibraryPath == "")
+            {
+                LibraryPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase).LocalPath);
+                LibraryPath = Path.Combine(LibraryPath, "library");
+            }
+            AddLibrary(Path.Combine(LibraryPath, "vendor", "list.xml"));
+            AddLibrary(Path.Combine(LibraryPath, "custom", "list.xml"));
+        }
+
+        private void AddLibrary(string FileName)
+        {
+            if (File.Exists(FileName))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(Library));
+                using (TextReader reader = new StreamReader(FileName))
+                {
+                    object obj = deserializer.Deserialize(reader);
+                    Add((Library)obj);
+                }
+            }
+        }
+    }
+
+
+    [XmlRoot("library")]
     public class Library
     {
+        public enum TypeLibrary
+        {
+            [XmlEnum(Name = "vendor")]
+            Vendor,
+            [XmlEnum(Name = "custom")]
+            Custom
+        }
+        [XmlAttribute("type")]
+        public TypeLibrary Type { get; set; }
+
+        [XmlArray("modules")]
+        [XmlArrayItem(ElementName = "module")]
+        public List<Module> ModuleList { get; set; }
+
+        [XmlIgnore]
+        public Dictionary<int, Module> ModuleDict
+        {
+            get { return ModuleList.ToDictionary(x => x.Uid, x => x); }
+            set { ModuleList = value.Values.ToList(); }
+
+        }
+        public Library() { }
+        public Library(TypeLibrary typeLibrary) { Type = typeLibrary; }
     }
+
+
+    public class Module
+    {
+
+        [XmlAttribute("uid")]
+        public int Uid { get; set; }
+
+        public enum TypeSignature
+        {
+            [XmlEnum(Name = "none")]
+            None,
+            [XmlEnum(Name = "auto")]
+            Auto,
+            [XmlEnum(Name = "rccu")]
+            Rccu
+        }
+        [XmlAttribute("type_signature")]
+        public TypeSignature Signature { get; set; }
+
+        public enum TypeBootloader
+        {
+            [XmlEnum(Name = "bl1")]
+            Bl1,
+            [XmlEnum(Name = "bl2")]
+            Bl2,
+            [XmlEnum(Name = "bl3")]
+            Bl3
+        }
+        [XmlAttribute("type_bootloader")]
+        public TypeBootloader Bootloader { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+
 }
--- Planar/Tests/TestLibrary/UnitTest.cs
+++ Planar/Tests/TestLibrary/UnitTest.cs
@@ -1,14 +1,69 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Planar.Library;
+
 namespace TestLibrary
 {
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        Libraries libraries;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            libraries = Libraries.Instance;
         }
+
+        [TestMethod]
+        public void TestInitializeLibraries()
+        {
+            Assert.AreNotEqual(libraries, null);
+        }
+
+        [TestMethod]
+        public void TestLibraries()
+        {
+            Assert.IsNotNull(libraries);
+            Assert.AreEqual(libraries.Count, 0);
+
+            // Добавить в коллекцию библиотек новую с указанием типа библиотеки (vendor, custom)
+            libraries.Add(new Library(Library.TypeLibrary.Custom));
+            libraries.Add(new Library(Library.TypeLibrary.Vendor));
+            Assert.AreEqual(libraries.Count, 2);
+
+            Library library;
+            Assert.IsTrue(libraries.TryGetValue(Library.TypeLibrary.Custom, out library));
+            Assert.IsNotNull(library);
+            library = null;
+
+            Assert.IsTrue(libraries.TryGetValue(Library.TypeLibrary.Vendor, out library));
+            Assert.IsNotNull(library);
+            library = null;
+
+            libraries.Remove(Library.TypeLibrary.Custom);
+            Assert.AreEqual(libraries.Count, 1);
+
+            Assert.IsFalse(libraries.TryGetValue(Library.TypeLibrary.Custom, out library));
+            Assert.IsNull(library);
+        }
+
+        [TestMethod]
+        public void TestSerialize()
+        {
+            Assert.IsNotNull(libraries);
+            Assert.AreEqual(libraries.Count, 0);
+
+            // Добавить в коллекцию библиотек новую с указанием типа библиотеки (vendor, custom)
+            libraries.Add(new Library(Library.TypeLibrary.Custom));
+            libraries.Add(new Library(Library.TypeLibrary.Vendor));
+
+            libraries.Serializere();
+          //  Assert.IsTrue(true);
+        }
+
+
+
     }
 }
