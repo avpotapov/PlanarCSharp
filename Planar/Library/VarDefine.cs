c1ce4ceab91d3f8f614cb72896c7d7356601a600
Алексей Потапов <al.ptp@hotmail.com>
2015-09-10 16:42:16 +0300

    Добавлен VarDefine

---
+++ Planar/Library/VarDefine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Planar.Library
+{
+
+    public enum TypeRegister
+    {
+        [XmlEnum(Name = "HOLDING")]
+        Holding,
+
+        [XmlEnum(Name = "INPUT")]
+        Input
+    }
+
+    [XmlType(TypeName ="var_define")]
+    public class VarDefine
+    {
+        public VarDefine() { }
+        public VarDefine(TypeRegister typeRegister)
+        {
+            TypeRegister = typeRegister;
+        }
+
+        [XmlAttribute("type")]
+        public TypeRegister TypeRegister { get; set; }
+
+        [XmlElement("name")]
+        [XmlText]
+        public string Name { get; set; }
+
+        public string Uid { get; set; }
+
+    }
+
+}
